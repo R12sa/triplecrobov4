e865471c4fdb37dd7dd43478a487f45d868ac2a5
R12sa <144062428+R12sa@users.noreply.github.com>
2025-03-31 16:57:24 -0700

    Update 606849621.lua

--- games/606849621.lua
+++ games/606849621.lua
@@ -1,20 +1,22 @@
 local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') end
-	return res
+    local res, err = loadstring(...)
+    if err and vape then vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') end
+    return res
 end
+
 local isfile = isfile or function(file)
-	local suc, res = pcall(function() return readfile(file) end)
-	return suc and res ~= nil and res ~= ''
+    local suc, res = pcall(function() return readfile(file) end)
+    return suc and res ~= nil and res ~= ''
 end
+
 local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function() return game:HttpGet('https://raw.githubusercontent.com/QP-Offcial/VapeV4ForRoblox/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) end)
-		if not suc or res == '404: Not Found' then error(res) end
-		if path:find('.lua') then res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
+    if not isfile(path) then
+        local suc, res = pcall(function() return game:HttpGet('https://raw.githubusercontent.com/R12sa/triplecrobov4/main/'..select(1, path:gsub('newvape/', '')), true) end)
+        if not suc or res == '404: Not Found' then error(res) end
+        if path:find('.lua') then res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res end
+        writefile(path, res)
+    end
+    return (func or readfile)(path)
 end
 local run = function(func) func() end
 local cloneref = cloneref or function(obj) return obj end
